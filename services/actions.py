6d68dd666cd919269fe02e254e1605177a9fa393
Aleksandr <iamobiwan@yandex.ru>
2023-04-14 14:58:20 +0300

    edit schedule

--- services/actions.py
+++ services/actions.py
@@ -19,78 +19,80 @@ async def check_pending_orders():
         orders = session.query(Order).filter(
             Order.status == 'pending',
             ).all()
-        for order in orders: # перебор всех заказов
-            user_state: FSMContext = dp.current_state(user=order.user.telegram_id, chat=order.user.chat_id) # достаем user_state
-            if check_order(order): # проверка оплаты заказа через API y_money
-                logger.info(f'Счет {order.id} успешно оплачен!')
-                user_data = await user_state.get_data()
-                if order.invite_discount: # был ли заказ по чьему-то промокоду?
-                    inviting_user = session.query(User).where(User.id == order.user.inviting_user_id).first()
-                    if inviting_user.discount >= settings.MAX_DISCOUNT: # у пользователя макс скидка?
-                        try:
-                            await bot.send_message(
-                            chat_id=inviting_user.chat_id,
-                            text=messages.MAX_DISCOUNT_FOR_USER,
-                                parse_mode='Markdown',
-                                reply_markup=back_main_keyboard()
-                            ) 
-                        except:
-                            pass
+        if orders:
+            for order in orders: # перебор всех заказов
+                user_state: FSMContext = dp.current_state(user=order.user.telegram_id, chat=order.user.chat_id) # достаем user_state
+                if check_order(order): # проверка оплаты заказа через API y_money
+                    logger.info(f'Счет {order.id} успешно оплачен!')
+                    user_data = await user_state.get_data()
+                    if order.invite_discount: # был ли заказ по чьему-то промокоду?
+                        inviting_user = session.query(User).where(User.id == order.user.inviting_user_id).first()
+                        if inviting_user.discount >= settings.MAX_DISCOUNT: # у пользователя макс скидка?
+                            try:
+                                await bot.send_message(
+                                chat_id=inviting_user.chat_id,
+                                text=messages.MAX_DISCOUNT_FOR_USER,
+                                    parse_mode='Markdown',
+                                    reply_markup=back_main_keyboard()
+                                ) 
+                            except:
+                                pass
+                        else:
+                            inviting_user.discount += settings.DISCOUNT_FOR_INVITE_USER
+                            try:
+                                await bot.send_message(
+                                chat_id=inviting_user.chat_id,
+                                text=messages.DISCOUNT_FOR_INVITING_USER,
+                                    parse_mode='Markdown',
+                                    reply_markup=back_main_keyboard()
+                                ) 
+                            except:
+                                pass
+                            session.add(inviting_user)
+                    order.status = 'success' # обновляем данные заказа и пользователя
+                    if order.user.status in ['expired','created', 'outdated']:
+                        order.user.expires_at = datetime.now() + timedelta(days=order.days)
                     else:
-                        inviting_user.discount += settings.DISCOUNT_FOR_INVITE_USER
-                        try:
-                            await bot.send_message(
-                            chat_id=inviting_user.chat_id,
-                            text=messages.DISCOUNT_FOR_INVITING_USER,
-                                parse_mode='Markdown',
-                                reply_markup=back_main_keyboard()
-                            ) 
-                        except:
-                            pass
-                        session.add(inviting_user)
-                order.status = 'success' # обновляем данные заказа и пользователя
-                if order.user.status in ['expired','created', 'outdated']:
-                    order.user.expires_at = datetime.now() + timedelta(days=order.days)
-                else:
-                    order.user.expires_at += timedelta(days=order.days)
-                order.user.status = 'subscribed'
-                order.user.updated_at = datetime.now()
-                order.updated_at = datetime.now()
-                await user_state.update_data( # обновляем данные в Redis
-                    status='subscribed',
-                    expires_at=order.user.expires_at.strftime("%d.%m.%Y")
-                )
-
-                try:
-                    await bot.send_message(
-                        chat_id=order.user.chat_id,
-                        text=messages.SUCCESS_ORDER.format(
-                            id=order.id,
-                            amount=order.amount,
-                            status=status.USER_STATUS.get(order.user.status),
-                            expires_at=order.user.expires_at.strftime("%d.%m.%Y")
-                        ),
-                        parse_mode='Markdown',
-                        reply_markup=back_main_keyboard()
-                    )
-                except:
-                    logger.warning(f'Сообщение об успешной оплате не отправлено пользователю {order.user.id}')
-
-            else: # если заказ не оплачен проверяем, протух ли он
-                diff: timedelta = datetime.now() - order.created_at
-                if diff.days > settings.PENDING_ORDER_TTL:
-                    order.deleted = True
-                    order.status = 'expired'
+                        order.user.expires_at += timedelta(days=order.days)
+                    order.user.status = 'subscribed'
+                    order.user.updated_at = datetime.now()
                     order.updated_at = datetime.now()
-                    await bot.send_message(
-                        chat_id=order.user.telegram_id,
-                        text=messages.DELETE_ORDER.format(
-                            id=order.id,
-                            amount=order.amount
-                            )
+                    await user_state.update_data( # обновляем данные в Redis
+                        status='subscribed',
+                        expires_at=order.user.expires_at.strftime("%d.%m.%Y")
+                    )
+
+                    try:
+                        await bot.send_message(
+                            chat_id=order.user.chat_id,
+                            text=messages.SUCCESS_ORDER.format(
+                                id=order.id,
+                                amount=order.amount,
+                                status=status.USER_STATUS.get(order.user.status),
+                                expires_at=order.user.expires_at.strftime("%d.%m.%Y")
+                            ),
+                            parse_mode='Markdown',
+                            reply_markup=back_main_keyboard()
                         )
-            session.add(order)
-            session.commit()
+                    except:
+                        logger.warning(f'Сообщение об успешной оплате не отправлено пользователю {order.user.id}')
+
+                else: # если заказ не оплачен проверяем, протух ли он
+                    diff: timedelta = datetime.now() - order.created_at
+                    if diff.days > settings.PENDING_ORDER_TTL:
+                        order.deleted = True
+                        order.status = 'expired'
+                        order.updated_at = datetime.now()
+                        await bot.send_message(
+                            chat_id=order.user.telegram_id,
+                            text=messages.DELETE_ORDER.format(
+                                id=order.id,
+                                amount=order.amount
+                                )
+                            )
+                session.add(order)
+                session.commit()
+            await rebuild_server_config()
 
 async def check_pending_vpn():
     logger.info('Проверка ожидающих VPN...')
@@ -185,3 +187,4 @@ async def check_sub_expire():
                         )
             session.add(user)
             session.commit()
+    await rebuild_server_config()
