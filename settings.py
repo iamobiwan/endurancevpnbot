ae56ee565a8a67ef222fa271434294be81f47bd0
Aleksandr <iamobiwan@yandex.ru>
2023-04-17 15:48:48 +0300

    update settings

--- settings.py
+++ settings.py
@@ -15,9 +15,9 @@ DONATE_URL = "https://yoomoney.ru/quickpay/confirm.xml?receiver={wallet}\
 
 PENDING_ORDER_TTL = 2
 
-MAX_SERVER_USER_COUNT = 30
+MAX_SERVER_USER_COUNT = 50
 
-SUB_DAYS_NOTIFICATION = [3,7]
+SUB_DAYS_NOTIFICATION = [1, 3, 7]
 
 EXPIRED_SUB = 83
 
