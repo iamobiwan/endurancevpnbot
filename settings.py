0fe2705cdf25e52da489dbd477e4a78231da8687
Aleksandr <iamobiwan@yandex.ru>
2023-04-26 11:48:35 +0300

    update settings

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 TRIAL_SUB_DAYS = 3
 
-DISCOUNT_FOR_NEW_USER = 3
+DISCOUNT_FOR_NEW_USER = 300 # сумма скидки в рублях для новых пользователей
 
-DISCOUNT_FOR_INVITE_USER = 1
+DISCOUNT_FOR_INVITE_USER = 100 # сумма скидки в рублях за приведенного друга
 
-MAX_DISCOUNT = 3
+MAX_DISCOUNT = 300 # максимальная скидка в рублях
 
 LEN_PROMOCODE = 6
 
@@ -13,12 +13,12 @@ MAX_ORDERS_CNT = 3
 DONATE_URL = "https://yoomoney.ru/quickpay/confirm.xml?receiver={wallet}\
 &quickpay-form=shop&sum={amount}&label={label}"
 
-PENDING_ORDER_TTL = 2
+PENDING_ORDER_TTL = 2 # время жизни счета на оплату
 
 MAX_SERVER_USER_COUNT = 50
 
 SUB_DAYS_NOTIFICATION = [1, 3, 7]
 
-EXPIRED_SUB = 3
+EXPIRED_SUB = 23 # дней до напоминания об удалении VPN
 
-OUDATED_SUB = 4
+OUDATED_SUB = 30 # дней до удаления VPN
