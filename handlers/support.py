3dd0c6faf82f768ccb8135b2d3dec21da013f5df
Aleksandr <iamobiwan@yandex.ru>
2023-04-26 12:06:36 +0300

    added support handler

---
+++ handlers/support.py
@@ -0,0 +1,15 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+
+from keyboards.inline.main import back_main_keyboard
+from misc import messages
+
+async def support(message: types.Message, state: FSMContext):
+    """ Главное меню (обработка c message handler)"""
+    await message.answer(
+        messages.SUPPORT,
+        parse_mode='Markdown',
+        reply_markup=back_main_keyboard())
+
+def register_main_handlers(dp : Dispatcher):
+    dp.register_message_handler(support, commands=['support'], state='*')
