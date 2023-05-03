0d67ff2a0a0a97d61cdfec8ad75aeed73f2072b0
Aleksandr <iamobiwan@yandex.ru>
2023-05-03 09:18:39 +0300

    add admin alert for sync config

--- services/sync.py
+++ services/sync.py
@@ -1,10 +1,12 @@
 from paramiko import SSHClient, AutoAddPolicy
 import os
 from loader import bot, logger, config
+from misc import messages
+from db.models import Server
 
 
 @logger.catch
-async def sync_config(server):
+async def sync_config(server: Server):
     logger.info(f'Начинаю синхронизацию конфигурации сервера {server.name}...')
     client = SSHClient()
     client.set_missing_host_key_policy(AutoAddPolicy())
@@ -19,11 +21,25 @@ async def sync_config(server):
         else:
             logger.warning('Не синхронизировано') 
     except TimeoutError:
-        logger.warning('Не удается подключиться к серверу, синхронизация не выполнена')
+        for admin_id in bot.get('config').tg_bot.admin_ids:
+            await bot.send_message(
+                chat_id=admin_id,
+                text=messages.ADMIN_SYNC_TIMEOUT_ERROR.format(server=server.name),
+                parse_mode='Markdown'
+            )
+            logger.warning(f'Не удается подключиться к серверу {server.name}, синхронизация не выполнена')
+    except:
+        for admin_id in bot.get('config').tg_bot.admin_ids:
+            await bot.send_message(
+                chat_id=admin_id,
+                text=messages.ADMIN_SYNC_ERROR.format(server=server.name),
+                parse_mode='Markdown'
+            )
+            logger.warning(f'Синхронизация сервера {server.name}не выполнена')
 
 
 @logger.catch
-async def check_config(server):
+async def check_config(server: Server):
     logger.info('Начинаю проверку конфигурации...')
     client = SSHClient()
     client.set_missing_host_key_policy(AutoAddPolicy())
@@ -42,5 +58,19 @@ async def check_config(server):
                     await bot.send_message(admin, f'Конфигурация не залилась на сервер {server.name}, ip: {server.wan_ip}')
                     logger.warning(f'Конфигурация сервера {server.name}, ip: {server.wan_ip} не обновилась!')
     except TimeoutError:
-        logger.warning('Не удается подключиться к серверу, проверка не выполнена')
+        for admin_id in bot.get('config').tg_bot.admin_ids:
+            await bot.send_message(
+                chat_id=admin_id,
+                text=messages.ADMIN_CHECK_TIMEOUT_ERROR.format(server=server.name),
+                parse_mode='Markdown'
+            )
+            logger.warning(f'Не удается подключиться к серверу {server.name}, проверка не выполнена')
+    except:
+        for admin_id in bot.get('config').tg_bot.admin_ids:
+            await bot.send_message(
+                chat_id=admin_id,
+                text=messages.ADMIN_CHECK_ERROR.format(server=server.name),
+                parse_mode='Markdown'
+            )
+            logger.warning(f'Проверка сервера {server.name} не выполнена')
 
